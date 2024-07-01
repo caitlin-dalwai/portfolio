16b7434dd0a9c3fa422dfbde1a672a56ee32c1e1
caitlin-dalwai <dalwaicaitlin@gmail.com>
2024-07-01 15:50:02 +0200

    nav bar

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProjectsView from '../views/ProjectsView.vue'
+import AboutView from '../views/AboutView.vue'
+import ContactView from '../views/ContactView.vue'
+import TestimonialsView from '../views/TestimonialsView.vue'
+import SkillsView from '../views/SkillsView.vue'
 
 const routes = [
   {
@@ -12,6 +16,26 @@ const routes = [
     path: '/projects',
     name: 'projects',
     component: ProjectsView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/skills',
+    name: 'skills',
+    component: SkillsView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+  {
+    path: '/testimonials',
+    name: 'testimonials',
+    component: TestimonialsView
   },
   {
     path: '/about',
